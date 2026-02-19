cdc940d0b7ebc51de58b9a8c358e799de6f144dd
SeanSales2023 <sean.sales@student.ateneo.edu>
2026-02-19 23:29:22 +0800

    Everything finished, templates and views updated

--- ledger/models.py
+++ ledger/models.py
@@ -4,7 +4,7 @@ class Ingredient(models.Model):
     ingredientName = models.CharField(max_length=100)
 
     def __str__(self):
-        return '{}'.format(self.ingredientName)
+        return f'{self.ingredientName}'
     
     def get_absolute_url(self):
         return reverse('recipes/list', args=[str(self.name)])
@@ -13,7 +13,7 @@ class Recipe(models.Model):
     recipeName = models.CharField(max_length=100)
 
     def __str__(self):
-        return '{}'.format(self.recipeName)
+        return f'{self.recipeName}'
     
     def get_absolute_url(self):
         return reverse('recipes/list', args=[str(self.name)])
@@ -22,5 +22,8 @@ class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=50)
     ingredients = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name = 'ingredients')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name = 'recipe')
+    
+    def get_absolute_url(self):
+        return reverse('recipes/list', args=[str(self.name)])
 
 # Create your models here.
--- ledger/views.py
+++ ledger/views.py
@@ -1,37 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from .models import RecipeIngredient
+from .models import Recipe, RecipeIngredient
 
 # Create your views here.
 
 def recipes_list(request):
-    recipe1 = RecipeIngredient.objects.filter(recipe__recipeName__exact='Recipe 1')
-    recipe2 = RecipeIngredient.objects.filter(recipe__recipeName__exact='Recipe 2')
+    recipe1 = Recipe.objects.get(recipeName__exact='Recipe 1')
+    recipe2 = Recipe.objects.get(recipeName__exact='Recipe 2')
     recipe1ingredients = RecipeIngredient.objects.filter(recipe__recipeName='Recipe 1')
     recipe2ingredients = RecipeIngredient.objects.filter(recipe__recipeName='Recipe 2')
     ctx =  {
     'recipe1': recipe1,
     'recipe2': recipe2,
-    'recipe2ingredients': recipe1ingredients,
+    'recipe1ingredients': recipe1ingredients,
     'recipe2ingredients': recipe2ingredients,
     }
     return render(request, "recipes/recipes_list.html", ctx)
 
 def recipe1(request):
-    recipe = RecipeIngredient.objects.filter(recipe__name='Recipe 1')
-    ingredients = RecipeIngredient.objects.filter(recipe__recipeName='Recipe 1')
+    recipe = Recipe.objects.get(recipeName='Recipe 1')
+    items = RecipeIngredient.objects.filter(recipe__recipeName='Recipe 1')
     ctx = {
         'recipe': recipe,
-        'ingredients': ingredients
+        'items': items
     }
     return render(request, 'recipes/recipe.html', ctx)
 
 def recipe2(request):
-    recipe = RecipeIngredient.objects.filter(recipe__name='Recipe 2')
-    ingredients = RecipeIngredient.objects.filter(recipe__recipeName='Recipe 2')
+    recipe = Recipe.objects.get(recipeName='Recipe 2')
+    items = RecipeIngredient.objects.filter(recipe__recipeName='Recipe 2')
     ctx = {
         'recipe': recipe,
-        'ingredients': ingredients
+        'items': items
     }
     return render(request, 'recipes/recipe.html', ctx)
