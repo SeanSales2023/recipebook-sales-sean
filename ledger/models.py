ea75af314a3d4b124ed33d990e7fe166522011ec
SeanSales2023 <sean.sales@student.ateneo.edu>
2026-02-19 21:29:30 +0800

    models done including functions

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+class Ingredient(models.Model):
+    ingredientName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.ingredientName)
+    
+    def get_absolute_url(self):
+        return reverse('recipes/list', args=[str(self.name)])
+
+class Recipe(models.Model):
+    recipeName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{}'.format(self.recipeName)
+    
+    def get_absolute_url(self):
+        return reverse('recipes/list', args=[str(self.name)])
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=50)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
 # Create your models here.
