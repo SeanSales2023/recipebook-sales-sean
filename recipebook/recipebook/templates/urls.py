a9a5350b57ba2ba4be0fcdaa3572673f67da3575
SeanSales2023 <sean.sales@student.ateneo.edu>
2026-03-12 22:28:23 +0800

    Wrong upload

--- recipebook/recipebook/templates/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from .views import ProductListView, ProductDetailView
-
-app_name = 'merchstore'
-
-urlpatterns = [
-path('items', ProductListView.as_view(), name='product-list'),
-path('item/<int:pk>', ProductDetailView.as_view(), name='product-detail')
-]
